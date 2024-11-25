2a035fd8c9e1cf490cc12413388e5ef19839c825
ddangmul <j.ddangmul@gmail.com>
2024-11-25 14:21:01 +0900

    [유효성 검사] forwardRef, useImperativeHandle 사용해 Modal 유연하게 만들기

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import Input from "./Input";
 import { useRef } from "react";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -18,6 +21,8 @@ export default function NewProject({ onAdd }) {
       enterdDueDate.trim() === ""
     ) {
       // show the error modal
+      modal.current.open();
+      return; // 정지 : 모달창 뜨면 onAdd함수 실행하지 않음
     }
 
     onAdd({
@@ -28,27 +33,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancle
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input ref={title} label="Title" />
-        <Input ref={description} label="Description" isTextarea />
-        <Input ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="확인">
+        <h2>입력값이 유효하지 않습니다.</h2>
+        <p>입력란이 모두 작성되지 않았습니다.</p>
+        <p>입력란에 유효한 입력값을 작성하세요.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancle
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} label="Title" />
+          <Input ref={description} label="Description" isTextarea />
+          <Input ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
