37603f25953b98951b4dfba8a9e136866f5af862
ddangmul <j.ddangmul@gmail.com>
2024-11-26 13:34:43 +0900

    Tasks&NewTask 컴포넌트 추가

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,3 +1,12 @@
+import NewTask from "./NewTask";
+
 export default function Tasks() {
-  return;
+  return (
+    <section className="text=2xl font-bold text-stone-700 mb-4">
+      <h2>할 일</h2>
+      <NewTask />
+      <p className="text-stone-800 my-4">아직 할 일 목록이 없습니다.</p>
+      <ul></ul>
+    </section>
+  );
 }
